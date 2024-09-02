8dc1241d06165bba8bb697d888ff1a819b0cdcec
purushotham <nagaruripurushotham123@gmail.com>
2024-09-02 21:35:56 +0530

    modified rag application

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from uuid import uuid4
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders.pdf import PyMuPDFLoader
@@ -9,41 +8,54 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import chromadb
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import Chroma
-
+from pinecone import Pinecone,ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
 
 qa_chain = None
+chunk_size = 700
+chunk_over_lap = 50
+list_pdf_text = None
+db = None
+embed_model = None
 
-load_dotenv(dotenv_path=f'{os.getcwd()}/helpers/.env')
+load_dotenv(dotenv_path=f'{os.getcwd()}/hlepers/.env')
 
-#Read pdf data
 def read_pdf(path):
+    '''
+    This function would read the pdf
+    '''
     pdf_load = PyMuPDFLoader(path)
     list_pdf_data = pdf_load.load()
+    global list_pdf_text
+    list_pdf_text = list_pdf_data
     text_data = ""
     for data in list_pdf_data:
         text_data+=data.page_content                                                                                                                                                                                                                                                                                                                                                               
     return text_data
 
-# chunk data
-def create_chunks(chunksize, chunkoverlap, text_data):
+def create_chunks(text_data):
+    '''
+    This function would create chunks
+    '''
     text_splitter = RecursiveCharacterTextSplitter(
-                            chunk_size = chunksize,
-                            chunk_overlap = chunkoverlap
+                            chunk_size = chunk_size,
+                            chunk_overlap = chunk_over_lap
                     )
     chunk_text_data = text_splitter.split_text(text_data)
     return chunk_text_data
 
-# text_data = read_pdf("C:\\Users\\AIFA USER 03.LAPTOP-G1Q1AF36\\Downloads\\Mayo Oshin & Nuno Campos - Learning LangChain (for Raymond Rhine)-O'Reilly Media (2024).pdf")
-
 def chroma_embeddings(pdf_data):
-    # generate embeddings
-    collection_name = str(uuid4())
-    chunks_text = create_chunks(1000, 50, pdf_data)
-    model = TransformerEmbedder('all-MiniLM-L6-v2')
+    '''
+    This function would generate embeddings
+    '''
+    collection_name = 'pdf_data'
+    chunks_text = create_chunks(pdf_data)
+    print(embed_model)
+    model = TransformerEmbedder(embed_model)
     vectors_embeddings = model.generate_embeddings(chunks_text)
     client = chromadb.Client()
-    collection = client.create_collection(collection_name)
-    collection.add(
+    collection = client.get_or_create_collection(collection_name)
+    collection.upsert(
         documents=chunks_text,
         embeddings=vectors_embeddings.tolist(),
         ids=[str(i)for i in range(len(vectors_embeddings))]
@@ -51,19 +63,64 @@ def chroma_embeddings(pdf_data):
     return collection_name
 
 def retrival_data(coll_name):
-    db_model = SentenceTransformerEmbeddings(model_name = "all-MiniLM-L6-v2")
-    retriver = Chroma(collection_name=coll_name, embedding_function=db_model).as_retriever()
+    db_model = SentenceTransformerEmbeddings(model_name = embed_model)
+    retriver = None
+    if db == 'pinecone':
+        retriver = PineconeVectorStore.from_existing_index(index_name=coll_name, embedding=db_model).as_retriever()
+    else:
+        retriver = Chroma(collection_name=coll_name, embedding_function=db_model).as_retriever()
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro",api_key=os.getenv('GOOGLE_API_KEY'),top_k=6)
     qa_chain = RetrievalQA.from_chain_type(llm = llm, retriever = retriver, chain_type = 'stuff')
     return qa_chain
  
+# gemini-1.5-pro
+# gemini-1.5-flash-001
 def upload_file(file):
     pdf_data = read_pdf(file.name)
-    collection_name = chroma_embeddings(pdf_data)
+    collection_name = ''
+    if db == 'pinecone':
+        collection_name = pinecone_db(pdf_data)
+    else:
+        collection_name = chroma_embeddings(pdf_data)
     global qa_chain
     qa_chain = retrival_data(collection_name)
 
-def user_input(user): 
+def user_input(user):
     response = qa_chain({"query":user})
-    print(response['result'])
     return response['result']
+
+def customisation(file, chunksize, chunk_overlap, database, embedding_model):
+    global chunk_size, chunk_over_lap, db, embed_model
+    chunk_size = chunksize
+    chunk_over_lap = chunk_overlap
+    db = database
+    embed_model = embedding_model
+    upload_file(file)
+
+def pinecone_db(pdf_data):
+    '''
+    This would store the vector embeddings in Pinecone database
+    '''
+    index_name = "docs-rag-testchatbot"
+    chunks_text = create_chunks(pdf_data)
+    print(embed_model)
+    model = TransformerEmbedder(model)
+    vectors_embeddings = model.generate_embeddings(chunks_text)
+    pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
+    # pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment='us-west1-gcp')
+    if index_name not in pc.list_indexes().names():
+        pc.create_index(
+            name=index_name,
+            dimension=384, 
+            metric="cosine", 
+            spec=ServerlessSpec(
+                cloud="aws", 
+                region="us-east-1"
+            ) 
+        )
+    index = pc.Index(index_name)
+    ids = [f'id_{i}' for i in range(len(chunks_text))]
+    metadata = [{'text' : data} for data in chunks_text]
+    upsert_data = list(zip(ids, vectors_embeddings, metadata))
+    index.upsert(vectors=upsert_data)
+    return index_name
--- view.py
+++ view.py
@@ -1,27 +1,39 @@
 import gradio as gr
-from main import user_input, upload_file
 
 def generate_output(prompt, history):
+    from main import user_input
     return user_input(prompt)
-
-def get_file_path(file):
-    return file.name
+    
+def total_customisation(file, chunk_range, chunk_over_lap, select_db, select_embedding_model):
+    from main import customisation
+    return customisation(file, chunk_range, chunk_over_lap, select_db, select_embedding_model)
 
 with gr.Blocks() as allign:
     with gr.Row():
         with gr.Column(scale=1): 
             gr.Markdown('# Upload File')
             pdf_file_input = gr.File(label='upload pdf')
-            pdf_file_input.change(
-                fn=upload_file,
-                inputs=pdf_file_input
+            gr.Markdown('## select chunk size')
+            chunk_range = gr.Slider(minimum=100, maximum=4000, step=1, label='chunk size')
+            gr.Markdown('## chunk overlap')
+            chunk_over_lap = gr.Slider(minimum=20, maximum=400, step=1, label='chunk over lap')
+            gr.Markdown('## Data Base')
+            select_db = gr.Radio(['pinecone', 'chromadb'], label='DataBases', info='Our services')
+            gr.Markdown('## EmbeddingModel')
+            select_embedding_model = gr.Dropdown(['all-MiniLM-L6-v2', 
+                                                  'paraphrase-multilingual-MiniLM-L12-v2', 
+                                                  'all-mpnet-base-v2', 'all-MiniLM-L12-v2'],
+                                                  label='embedding models')
+            submit_button = gr.Button('Submit')
+            submit_button.click(
+                fn=total_customisation,
+                inputs=[pdf_file_input, chunk_range, chunk_over_lap, select_db, select_embedding_model]
             )
+
         with gr.Column(scale=4):
             iface = gr.ChatInterface(
                 fn=generate_output,
-                chatbot=gr.Chatbot(
-                    placeholder='<h1 style="color : blue;font-size: 77px;">Welcome<h1>'
-                ),
+                chatbot=gr.Chatbot(),
                 title="ChatWithYourBooks",
                 description="Discover, Discuss, and Delight in Every Page"
             )
